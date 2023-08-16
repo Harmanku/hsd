dfd5cf3c4dac1051191f5ba22a7d986d71c0e8d9
Harmanku <52563461+Harmanku@users.noreply.github.com>
2023-08-15 23:23:14 -0700

    project started

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors:{
+        gold: '#CDA34F',
+        daisy: '#E9E7DA',
+        Stem: '#636B46',
+        Greenery: '#373F27',
+        textWhite: '#FFECC7',
+        textBlack: '#2C3718'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
