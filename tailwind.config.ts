535c5c61c2d16fd8084bb313c5b479615ca06578
Harmanku <52563461+Harmanku@users.noreply.github.com>
2023-08-24 17:22:54 -0700

    24
    This reverts commit d68d0712a973ccc6db9d2410b690c8f59bd3cf65.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  
 
-  important: true,
+  
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/dashboard/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/logIn/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',    
+    
     
   ],
   theme: {
